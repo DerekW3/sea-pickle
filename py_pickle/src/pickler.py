451fd22b0bad15f0b695dc936dd9e3e13b7ad69e
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-10-31 21:34:03 -0500

    Feat(Pickler): Implement None pickler

--- py_pickle/src/pickler.py
+++ py_pickle/src/pickler.py
@@ -1,6 +1,18 @@
 import pickle
 import pickletools
 
+NONE: bytes = b"N"
+TRUE: bytes = b"\x88"
+FALSE: bytes = b"\x89"
+
+
+class Pickler:
+    def __init__(self):
+        print("Pickler prepared, starting now...")
+
+    def encode_none(self) -> bytes:
+        return NONE
+
 
 def encode_str(input_string: str):
     print("hello")
