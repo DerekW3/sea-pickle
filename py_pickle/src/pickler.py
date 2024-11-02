130c7eefde84f465e0cf9aadbc941e6b99366a40
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-11-01 21:40:04 -0500

    Refactor(Pickler): Rewrite write() method to write to self

--- py_pickle/src/pickler.py
+++ py_pickle/src/pickler.py
@@ -1,48 +1,47 @@
 from struct import pack
-from typing import Any
 
 from .opcodes.opcodes import OPCODE
 
 
 class Pickler:
     def __init__(self) -> None:
+        self.pickled = b""
         self.byte_count: int = 0
         self.codes = OPCODE()
         print("Pickler prepared, starting now...")
 
-    def pack_n_serve(self, pack_arg: str, bin_code: bytes, obj: Any) -> bytes:
-        packed: bytes = pack(pack_arg, obj)
-        concat: bytes = bin_code + packed
-        self.byte_count += len(concat)
-        return concat
+    def write(self, obj: bytes):
+        self.pickled += obj
+        self.byte_count = len(self.pickled)
 
-    def encode_none(self) -> bytes:
-        self.byte_count += 1
-        return self.codes.NONE
+    def encode_none(self):
+        self.write(self.codes.NONE)
 
-    def encode_bool(self, obj: bool) -> bytes:
-        self.byte_count += 1
-        return self.codes.TRUE if obj else self.codes.FALSE
+    def encode_bool(self, obj: bool):
+        self.write(self.codes.TRUE if obj else self.codes.FALSE)
 
-    def encode_string(self, obj: str) -> bytes:
+    def encode_string(self, obj: str):
         utf_string: bytes = obj.encode("utf-8", "surrogatepass")
         length: int = len(utf_string)
         if length < 256:
-            return self.pack_n_serve("<B", self.codes.SHORT_UNICODE, length)
+            self.write(self.codes.SHORT_UNICODE + pack("<B", length) + utf_string)
         elif length > 0xFFFFFFFF:
-            return self.pack_n_serve("<Q", self.codes.LONG_UNICODE, length)
+            self.write(self.codes.LONG_UNICODE + pack("<Q", length) + utf_string)
         else:
-            return self.pack_n_serve("<I", self.codes.UNICODE, length)
+            self.write(self.codes.UNICODE + pack("<I", length) + utf_string)
 
-    def encode_long(self, obj: int) -> bytes:
+    def encode_long(self, obj: int):
         if obj >= 0:
             if obj <= 0xFF:
-                return self.pack_n_serve("<B", self.codes.BININT1, obj)
+                self.write(self.codes.BININT1 + pack("<B", obj))
+                return
             elif obj <= 0xFFFF:
-                return self.pack_n_serve("<H", self.codes.BININT2, obj)
+                self.write(self.codes.BININT2 + pack("<H", obj))
+                return
 
         if -0x80000000 <= obj <= 0x7FFFFFFF:
-            return self.pack_n_serve("<i", self.codes.BININT, obj)
+            self.write(self.codes.BININT + pack("<i", obj))
+            return
 
         encoded_long: bytes = b""
 
@@ -61,6 +60,6 @@ class Pickler:
         n = len(encoded_long)
 
         if n < 256:
-            return self.pack_n_serve("<B", self.codes.LONG1, n) + encoded_long
+            self.write(self.codes.LONG1 + pack("<B", n) + encoded_long)
         else:
-            return self.pack_n_serve("<i", self.codes.LONG4, n) + encoded_long
+            self.write(self.codes.LONG4 + pack("<i", n) + encoded_long)
