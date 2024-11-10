e3ddca253e92fb672738b8f2c69da805fe6a5df7
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-11-10 14:39:11 -0600

    Feat(Pickler): Add partial_pickle function skeleton

--- sea_pickle/src/sea_pickle/sea_pickle.c
+++ sea_pickle/src/sea_pickle/sea_pickle.c
@@ -1,1 +1,13 @@
 #include "sea_pickle.h"
+#include <modsupport.h>
+#include <object.h>
+
+PyObject *partial_pickle(PyObject *self, PyObject *args) {
+  PyObject *obj;
+
+  if (!PyArg_ParseTuple(args, "O", &obj)) {
+    return NULL;
+  }
+
+  Py_RETURN_NONE;
+}
