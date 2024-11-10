18b14cf07919b005b343e986f8c35514721e4e64
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-11-10 15:02:00 -0600

    Build(Pickler): Add function skeletons in .c file

--- sea_pickle/src/sea_pickle/sea_pickle.c
+++ sea_pickle/src/sea_pickle/sea_pickle.c
@@ -27,4 +27,59 @@ PyObject *merge_partials(PyObject *self, PyObject *args) {
   Py_RETURN_NONE;
 }
 
-static PyObject *get_chunks(PyBytesObject *obj) { Py_RETURN_NONE; }
+static PyListObject *get_chunks(PyBytesObject *obj) { Py_RETURN_NONE; }
+
+static PyDictObject *get_memo(PyListObject *chunks) { Py_RETURN_NONE; }
+
+static PyBytesObject *listize(PyDictObject *memory, PyBytesObject *obj1,
+                              PyBytesObject *obj2) {
+  Py_RETURN_NONE;
+}
+
+static PyBytesObject *extract_tuple(PyListObject *chunks, PyObject *idx) {
+  Py_RETURN_NONE;
+}
+
+static PyBytesObject *extract_sequence(PyListObject *chunks, PyObject *idx) {
+  Py_RETURN_NONE;
+}
+
+static PyBytesObject *length_packer(PyObject *length) { Py_RETURN_NONE; }
+
+static PyBytesObject *get(PyObject *idx) { Py_RETURN_NONE; }
+
+static PyBytesObject *merge_strings(PyBytesObject *str_1,
+                                    PyBytesObject *identifier_1,
+                                    PyBytesObject *str_2,
+                                    PyBytesObject *identifier_2) {
+  Py_RETURN_NONE;
+}
+
+static PyBytesObject *merge_bytes(PyBytesObject *byte_str_1,
+                                  PyBytesObject *identifier_1,
+                                  PyBytesObject *byte_str_2,
+                                  PyBytesObject *identifier_2) {
+  Py_RETURN_NONE;
+}
+
+static PyBytesObject *encode_none(PyObject *obj) { Py_RETURN_NONE; }
+
+static PyBytesObject *encode_bool(PyObject *obj) { Py_RETURN_NONE; }
+
+static PyBytesObject *encode_string(const char *obj) { Py_RETURN_NONE; }
+
+static PyBytesObject *encode_float(PyFloatObject *obj) { Py_RETURN_NONE; }
+
+static PyBytesObject *encode_long(PyObject *obj) { Py_RETURN_NONE; }
+
+static PyBytesObject *encode_bytes(PyBytesObject *obj) { Py_RETURN_NONE; }
+
+static PyBytesObject *encode_tuple(PyTupleObject *obj) { Py_RETURN_NONE; }
+
+static PyBytesObject *encode_list(PyListObject *obj) { Py_RETURN_NONE; }
+
+static PyBytesObject *encode_dict(PyDictObject *obj) { Py_RETURN_NONE; }
+
+static PyBytesObject *add_batch(PyObject *items) { Py_RETURN_NONE; }
+
+static PyBytesObject *set_batch(PyObject *items) { Py_RETURN_NONE; }
