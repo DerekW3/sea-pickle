5d1bb8c3a5e1cbc61c9f03da9d6ecf2c33f1c0ba
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-11-09 22:22:00 -0600

    Feat(OPCODES): Implement OPCODES Enum in C

---
+++ sea_pickle/src/sea_pickle/sea_pickle.c
@@ -0,0 +1,50 @@
+#include <stdint.h>
+
+typedef enum {
+  MEMO = 0x94,
+  BINGET = 'h',
+  LONG_BINGET = 'j',
+
+  // BOOL-LIKE TYPES
+  NONE = 'N',
+  TRUE = 0x88,
+  FALSE = 0x89,
+
+  // STRING TYPES
+  SHORT_UNICODE = 0x8c,
+  UNICODE = 'X',
+  LONG_UNICODE = 0x8d,
+
+  // NUMERIC TYPES
+  BININT = 'J',
+  BININT1 = 'K',
+  BININT2 = 'M',
+  LONG1 = 0x8a,
+  LONG4 = 0x8b,
+  BINFLOAT = 'G',
+
+  // BYTES TYPES
+  SHORT_BINBYTES = 'C',
+  BINBYTES8 = 0x8e,
+  BINBYTES = 'B',
+
+  // ARRAY TYPES
+  MARK = '(',
+  APPENDS = 'e',
+  APPEND = 'a',
+
+  // DICT TYPES
+  EMPTY_DICT = '}',
+  SETITEM = 's',
+  SETITEMS = 'u',
+
+  // TUPLE TYPES
+  EMPTY_TUPLE = ')',
+  TUPLE1 = 0x85,
+  TUPLE2 = 0x86,
+  TUPLE3 = 0x87,
+  TUPLE = 't',
+
+  // LIST TYPES
+  EMPTY_LIST = ']'
+} OPCODES;
