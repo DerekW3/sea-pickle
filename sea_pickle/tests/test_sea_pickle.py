f9901d6d359f94c490bc5769199539dafe52b6ff
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-11-12 10:44:13 -0600

    Chore(Cleanup): Clean up binary files

--- sea_pickle/tests/test_sea_pickle.py
+++ sea_pickle/tests/test_sea_pickle.py
@@ -14,3 +14,15 @@ def test_partial_pickle():
     assert sea_pickle.partial_pickle(-100) in pickle.dumps(-100)
     assert sea_pickle.partial_pickle(1.05) in pickle.dumps(1.05)
     assert sea_pickle.partial_pickle(-1.05) in pickle.dumps(-1.05)
+
+    assert sea_pickle.partial_pickle("hello there") in pickle.dumps("hello there")
+    assert sea_pickle.partial_pickle("hi my name is") in pickle.dumps("hi my name is")
+    multiline_str = """
+    This is a multiline string
+    which tests my pickles capability to encode
+    unicode characters like the newline
+    """
+    assert sea_pickle.partial_pickle(multiline_str) in pickle.dumps(multiline_str)
+    assert sea_pickle.partial_pickle("") in pickle.dumps("")
+    assert sea_pickle.partial_pickle("a" * 255) in pickle.dumps("a" * 255)
+    assert sea_pickle.partial_pickle("a" * 256) in pickle.dumps("a" * 256)
