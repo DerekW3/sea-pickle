32509ed18e1be6c2e55b61302328b93692a03752
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-11-17 10:30:05 -0600

    Build(sea_pickle): Update build script

--- sea_pickle/scripts/build_script.sh
+++ sea_pickle/scripts/build_script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo "Building project..."
+echo "Cleaning project..."
 python setup.py clean --all
 
 echo "Deleting dirs..."
 rm -rf ./build ./sea_pickle.egg-info
+
+echo "Building project..."
+python setup.py build
 
 echo "Installing packages..."
 pip install .
